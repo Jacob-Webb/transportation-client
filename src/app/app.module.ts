eb7dfc608791dfd9d2d0e251f55c7f193b8d3fcf
Jacob Webb <jacob@jacob-webb.com>
2021-08-31 13:18:47 -0700

    added guards interceptors models modules and services directories

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavComponent } from './nav/nav.component';
+import { UserComponent } from './_models/user/user/user.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    NavComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
