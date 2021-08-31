452892f2d3851df55214fb9fa8414e0ae2b57d26
Jacob Webb <jacob@jacob-webb.com>
2021-08-31 13:42:37 -0700

    added modules to shared modules file

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { BasicAuthenticationService } from '../service/basic-authentication.service';
+import { AUTHENTICATED_USER, FIRST_NAME, StorageService, USER_ROLE } from '../service/data/storage.service';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.scss']
 })
-export class NavComponent implements OnInit {
+export class NavComponent {
+  userName: string;
+  role: string;
+  name: string;
+  email: string;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(public basicAuthenticationService: BasicAuthenticationService,
+              private storageService: StorageService) {
+    this.storageService.watchStorageItem(AUTHENTICATED_USER).subscribe(data => this.userName = data);
+    this.storageService.watchStorageItem(FIRST_NAME).subscribe(data => this.name = data);
+    this.storageService.watchStorageItem(USER_ROLE).subscribe(data => this.role = data);
   }
 
+  ngOnInit(): void {}
 }
