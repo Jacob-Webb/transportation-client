e9570293e553d53b0a0a1c132baa59898292cadd
Jacob Webb <jacob@jacob-webb.com>
2021-09-01 07:26:57 -0700

    created folder and components for errors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,13 +10,19 @@ import { NavComponent } from './globals/nav/nav.component';
 
 import { SharedModule } from './_modules/shared/shared.module';
 import { FooterComponent } from './globals/footer/footer.component';
+import { NotFoundComponent } from './errors/not-found/not-found.component';
+import { ServerErrorComponent } from './errors/server-error/server-error.component';
+import { TestErrorsComponent } from './errors/test-errors/test-errors.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
-    FooterComponent
+    FooterComponent,
+    NotFoundComponent,
+    ServerErrorComponent,
+    TestErrorsComponent
   ],
   imports: [
     BrowserModule,
