40fd589184f6f603b6d60e252a900a00e688e7d1
Jacob Webb <jacob@jacob-webb.com>
2021-09-01 10:03:23 -0700

    account service set up

--- src/app/access/register/register.component.ts
+++ src/app/access/register/register.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { IConfig } from 'ngx-mask';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-register',
