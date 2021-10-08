bab0fcdb0204833412b682456825da1cf39a7438
Jacob Webb <jacob@jacob-webb.com>
2021-10-08 06:10:37 -0700

    separated modules into different folders inside modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,12 +10,12 @@ import { NavComponent } from './core/header/nav.component';
 
 import { SharedModule } from './shared/shared.module';
 import { FooterComponent } from './core/footer/footer.component';
-import { NotFoundComponent } from './modules/pages/errors/not-found/not-found.component';
-import { ServerErrorComponent } from './modules/pages/errors/server-error/server-error.component';
-import { AccessComponent } from './modules/pages/access/access.component';
-import { RegisterComponent } from './modules/pages/access/register/register.component';
-import { SigninComponent } from './modules/pages/access/signin/signin.component';
-import { HomeComponent } from './modules/pages/home/home.component';
+import { NotFoundComponent } from './modules/errors/pages/errors/not-found/not-found.component';
+import { ServerErrorComponent } from './modules/errors/pages/errors/server-error/server-error.component';
+import { AccessComponent } from './modules/access/pages/access/access.component';
+import { RegisterComponent } from './modules/access/pages/access/register/register.component';
+import { SigninComponent } from './modules/access/pages/access/signin/signin.component';
+import { HomeComponent } from './modules/home/pages/home/home.component';
 
 import { NgxMaskModule } from 'ngx-mask';
 import { ErrorInterceptor } from './core/interceptors/error.interceptor';
