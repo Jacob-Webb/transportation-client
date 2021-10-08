223cf66d2eab5a97bd753f05971c41c91239cc0b
Jacob Webb <jacob@jacob-webb.com>
2021-10-08 05:52:31 -0700

    mremoved modules/home and moved pages to module/pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { NotFoundComponent } from './modules/home/pages/errors/not-found/not-found.component';
-import { AccessComponent } from './modules/home/pages/access/access.component';
-import { HomeComponent } from './modules/home/pages/home/home.component';
-import { ServerErrorComponent } from './modules/home/pages/errors/server-error/server-error.component';
+import { NotFoundComponent } from './modules/pages/errors/not-found/not-found.component';
+import { AccessComponent } from './modules/pages/access/access.component';
+import { HomeComponent } from './modules/pages/home/home.component';
+import { ServerErrorComponent } from './modules/pages/errors/server-error/server-error.component';
 
 const routes: Routes = [
   { path: '', component: AccessComponent },
