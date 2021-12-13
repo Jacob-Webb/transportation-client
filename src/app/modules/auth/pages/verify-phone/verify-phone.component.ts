9733c12ba23ff52c6d3253d406153f7a57088d92
Jacob Webb <jacob@jacob-webb.com>
2021-12-12 21:16:00 -0800

    Route from verify-phone to phone-confirmation or update-password depending on previousUrl

--- src/app/modules/auth/pages/verify-phone/verify-phone.component.ts
+++ src/app/modules/auth/pages/verify-phone/verify-phone.component.ts
@@ -3,11 +3,9 @@ import { Router } from '@angular/router';
 import { IConfig } from 'ngx-mask';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthenticationService } from 'src/app/core/authentication/authentication.service';
-import { UserForRegistrationDto } from 'src/app/shared/models/user';
 import { PhoneVerificationDto } from 'src/app/shared/models/phone-verification';
-import { PHONE_CONFIRMATION_URL } from 'src/app/app.constants';
+import { PHONE_VERIFICATION_URL } from 'src/app/app.constants';
 import { UrlService } from 'src/app/core/services/url.service';
-import { Observable } from 'rxjs';
 
 export let options: Partial<IConfig> | (() => Partial<IConfig>);
 
@@ -23,9 +21,9 @@ export class VerifyPhoneComponent implements OnInit {
   private responseData: string = '';
   private previousUrl: string | null = null;
 
-  constructor(private authService: AuthenticationService, 
-    private urlService: UrlService,
+  constructor(private authService: AuthenticationService,
     private router: Router,
+    private urlService: UrlService,
     fb: FormBuilder) { 
       this.verifyPhoneForm = fb.group ({
         'verifyCode':['', Validators.compose([Validators.maxLength(this.codeLength), Validators.required])]
@@ -35,12 +33,12 @@ export class VerifyPhoneComponent implements OnInit {
   ngOnInit(): void {
     this.responseData = history.state.data;
     if (this.responseData == '' || this.responseData == undefined) 
-      this.router.navigate(['auth']);
+      this.router.navigate(['auth']); 
 
     this.urlService.previousUrl$.subscribe((previousUrl: string | null) => {
-      console.log('previous url: ', previousUrl);
       this.previousUrl = previousUrl;
-    }) 
+      console.log(this.previousUrl);
+    })
   }
 
   public verify() {
@@ -49,20 +47,17 @@ export class VerifyPhoneComponent implements OnInit {
       phoneNumber: this.responseData
     }
 
-    if (this.previousUrl == '/forgot-password') {
-
-    }
-
-    if (this.previousUrl == '/register') {
-      this.authService.verifyPhone(PHONE_CONFIRMATION_URL, phoneVerificationDto)
-      .subscribe(response => {
-        console.log(response);
+    this.authService.verifyPhone(PHONE_VERIFICATION_URL, phoneVerificationDto)
+    .subscribe(response => {
+      if (this.previousUrl == '/register') {
         this.router.navigate(['phone-confirmation'], {state: {data: phoneVerificationDto.phoneNumber}});
-      }, error => {
-        console.log(error);
-        this.validationErrors = error;
-      })
-    }
+      }
+      if (this.previousUrl == '/forgot-password') {
+        this.router.navigate([''], {state: {data: phoneVerificationDto.phoneNumber}});
+      }
+    }, error => {
+      console.log(error);
+      this.validationErrors = error;
+    })
   }
-
 }
