d93587120bab25ed82980cbcd8ee7ca056cee04e
Jacob Webb <jacob@jacob-webb.com>
2021-12-15 14:13:47 -0800

    Added documentation via compodoc

--- src/app/shared/models/jwt-token.ts
+++ src/app/shared/models/jwt-token.ts
@@ -1,4 +1,16 @@
+/**
+ * Data transfer object to accept and pass tokens to the backend
+ */
 export interface JwtTokenDto {
+  /**
+   * The access token represents the short-lived token passed back and forth 
+   * to the backend to verify a user's authentication, thereby allowing access. 
+   */
   accessToken: string;
+
+  /**
+   * The refresh token represents the long-lived token persisting in the user's 
+   * account allowing for renewed access tokens as long as it is valid and not expired.
+   */
   refreshToken: string;
 }
